be7f68017e4d02472e49bc838fc9bd7817d624ca
simmedia <simadurlan92@gmail.com>
2019-11-12 12:23:51 +0100

    upd

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,7 @@
 
     <v-app-bar app clipped-left>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>SIMMEDIA</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
